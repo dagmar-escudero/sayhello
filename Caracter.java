2a3620b9ea58e98183c9f77ae504eb1b04b60783
dagmar-escudero <20243ds124@utez.edu.mx>
2025-05-09 00:06:35 -0600

    sayhello

---
+++ Caracter.java
@@ -0,0 +1,12 @@
+public class caracteres{
+    public static void main(string [] args) {
+        char letra = 'a';
+        char simbolo = '$';
+        char numerochar = '5';
+
+    System.out.println("letra =" + letra);
+            System.out.println("simbolo =" + simbolo);
+            System.out.println("numerochar =" + numerochar);
+
+    }
+}
